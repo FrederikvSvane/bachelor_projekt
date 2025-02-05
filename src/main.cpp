5c8659edc9c25d7dc1fa59dfdc835bd0fd68c294
FrederikvSvane <s224766@student.dtu.dk>
2025-02-05 14:13:25 +0100

    Cmake set up

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    std::cout << "Hello world" ;
+    std::cout << ", and mom" ;
+    
+    std::cout << std::endl;
+    return 0;
+}
