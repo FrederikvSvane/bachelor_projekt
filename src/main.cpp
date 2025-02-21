4a82e2ccd5a7c635d7b9b27b53a7d57b2fde4869
Marcus Christoffersen <s224750@dtu.dk>
2025-02-21 16:11:42 +0100

    cleaned up main with removal of std::

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,24 @@
 #include "domain/graph/graph.hpp"
 #include "domain/schedule.hpp"
 #include "service/graph/coloring.hpp"
-#include "service/graph/matching.hpp"
 #include "utils/argparse.hpp"
 #include "utils/json.hpp"
 #include "utils/parser.hpp"
-#include "utils/test_data_generator.hpp"
-#include <filesystem>
 #include <fstream>
 #include <iostream>
 
 using json = nlohmann::json;
 using namespace std;
-namespace fs = std::filesystem;
+namespace fs = filesystem;
 
 // Inline function to parse command-line arguments
 inline void parseArguments(int argc, char const *argv[], argparse::ArgumentParser &program) {
     program.add_argument("-i", "--input")
             .help("Path to the input JSON file")
-            .default_value(std::string(""));
+            .default_value(string(""));
     program.add_argument("-o", "--output")
             .help("Path to the output JSON file")
-            .default_value(std::string("output.json"));
+            .default_value(string("output.json"));
     program.add_argument("--test")
             .help("Use the test input file")
             .default_value(false)
@@ -29,14 +26,14 @@ inline void parseArguments(int argc, char const *argv[], argparse::ArgumentParse
 
     try {
         program.parse_args(argc, argv);
-    } catch (const std::exception &e) {
+    } catch (const exception &e) {
         cerr << "Argument parsing error: " << e.what() << endl;
         exit(1);
     }
 }
 
 // Inline function to write schedule output to a file
-inline void writeOutputFile(const std::string &filename, const json &output_json) {
+inline void writeOutputFile(const string &filename, const json &output_json) {
     ofstream out_file(filename);
     if (!out_file.is_open()) {
         cerr << "Error opening output file: " << filename << "\n";
